c350caa1c9e9b4d12f2f64584c578aba23a8a667
dlagmlwkd86-tech <dlagmlwkd86@gmail.com>
2026-04-04 03:10:29 +0900

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import ErrorBoundary from './ErrorBoundary'
-import './styles.css'
-import { ToastProvider } from './toast'
-import { ConfirmProvider } from './confirm'
-import { ThemeProvider } from './theme'
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch(() => {})
-  })
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <ToastProvider>
-        <ConfirmProvider>
-          <ErrorBoundary>
-            <App />
-          </ErrorBoundary>
-        </ConfirmProvider>
-      </ToastProvider>
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 )
